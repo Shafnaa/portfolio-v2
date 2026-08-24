f4980c536ece78df8e9ef689fe0424d7d8b06b1b
Saujana Shafi <hello@saujanashafi.com>
2026-08-24 09:18:17 +0700

    chore: Sidebar header component

---
+++ src/components/layout/app-sidebar/app-sidebar-header.tsx
@@ -0,0 +1,31 @@
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar";
+
+import { LogoIcon } from "@/components/icons";
+
+export function AppSidebarHeader({ ...props }: React.ComponentProps<typeof SidebarMenu>) {
+  const { toggleSidebar } = useSidebar();
+
+  return (
+    <SidebarMenu {...props}>
+      <SidebarMenuItem >
+        <SidebarMenuButton
+          size="lg"
+          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          onClick={toggleSidebar}
+        >
+          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+            <LogoIcon className="size-4" />
+          </div>
+          <div className="grid flex-1 text-left text-sm leading-tight">
+            <span className="truncate font-medium">Saujana Shafi</span>
+          </div>
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
