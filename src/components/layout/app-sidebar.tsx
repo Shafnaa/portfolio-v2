b9e14762467556edbbe39e377302125b62f83414
Saujana Shafi <hello@saujanashafi.com>
2026-08-24 09:18:34 +0700

    chore: Apply to AppSidebar

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -6,10 +6,14 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar"
 
+import { AppSidebarHeader } from "./app-sidebar/app-sidebar-header"
+
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader />
+      <SidebarHeader>
+        <AppSidebarHeader />
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup />
         <SidebarGroup />
